22267c6a2d9e4c7cca64ab01f0eaca09f8ce5995
Ali Bas <alibas01@gmail.com>
2021-06-10 21:34:19 -0400

    db migrate

--- db/migrate/20210611012928_create_tuuits.rb
+++ db/migrate/20210611012928_create_tuuits.rb
@@ -1,7 +1,7 @@
 class CreateTuuits < ActiveRecord::Migration[6.1]
   def change
     create_table :tuuits do |t|
-      t.integer :id
+      t.integer :tuuit_id
       t.integer :user_id
       t.timestamp :time
       t.text :tuuit
