48a6c6ea5776508322c2410f34f530f293773a6e
Ali Bas <alibas01@gmail.com>
2021-06-18 23:59:59 -0400

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  #get 'home/index'
+  root 'home#index'
+  get 'home/about'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/register' => 'users#new'
+  post '/users' => 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
