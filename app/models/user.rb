a8f7b090b16d8f084a98a26085bbb5e4c9318db0
Ali Bas <alibas01@gmail.com>
2021-06-20 14:06:05 -0400

    hashed password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ApplicationRecord
+  has_secure_password 
+
+  before_validation :strip_whitespace
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, :length => {:minimum => 4}
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, pass)
+    email = email.downcase
+    user = User.find_by_email(email)
+    if user && user.authenticate(pass)
+      user
+    else
+      nil
+    end
+  end 
+  
+  def strip_whitespace
+    self.name&.strip!
+    self.email&.strip!
+  end
 end
