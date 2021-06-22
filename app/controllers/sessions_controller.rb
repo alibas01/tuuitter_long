7ad55dc067c5483890adce5638780422b60e0a8e
Ali Bas <alibas01@gmail.com>
2021-06-22 12:19:31 -0400

    login page both functional and desired files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,16 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to root_path
     else
-      redirect_to '/register'
+      message = "Something went wrong! Please check your credentials!"
+      redirect_to login_path, notice: message
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to root_path
   end
+
 end
