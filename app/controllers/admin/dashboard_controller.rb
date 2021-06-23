cdaff8c6b06d5a3a1d5583acd141eacf84b728de
Ali Bas <alibas01@gmail.com>
2021-06-23 09:53:17 -0400

    admin authorization

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name:ENV['ADMIN_USERNAME'], password:ENV['ADMIN_PASSWORD']
 
   def show
-    @user_count = User.count
+    @user_count =User.count
     @post_count = Post.count
   end
 end
